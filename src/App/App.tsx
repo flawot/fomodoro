82db058c8bcc5f94674b9fde930c972636e5f3f1
flawot <sm4311089@gmail.com>
2026-02-16 09:43:38 +0500

    Инициализация

---
+++ src/App/App.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router";
+import Home from "../pages/home/Home";
+
+const App = () => {
+  return <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>;
+};
+
+export default App;
